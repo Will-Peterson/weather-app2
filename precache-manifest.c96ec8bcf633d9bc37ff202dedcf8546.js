2f5375c7e49934c924681860542bc3390918c6f5
Will Peterson <Will@Fortress.local>
2020-10-01 05:01:04 -0700

    Updates

---
+++ precache-manifest.c96ec8bcf633d9bc37ff202dedcf8546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a343e07fb3efef0537e2731fb850bc",
+    "url": "/weather-app2/index.html"
+  },
+  {
+    "revision": "cd89d8417b8e05885e4a",
+    "url": "/weather-app2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8da8e89140706a06e46",
+    "url": "/weather-app2/static/js/2.346be9b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app2/static/js/2.346be9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd89d8417b8e05885e4a",
+    "url": "/weather-app2/static/js/main.b718fadb.chunk.js"
+  },
+  {
+    "revision": "d5dc27778007689bf3dc",
+    "url": "/weather-app2/static/js/runtime-main.5a9569fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
